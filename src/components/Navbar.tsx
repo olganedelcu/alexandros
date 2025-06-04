66698530db49603efee6a02474d0cdd7929338a4
Olga <olganedelcuam@gmail.com>
2025-06-04 21:57:00 +0300

    everzthign

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,6 +10,7 @@ const Navbar = () => {
   const [visible, setVisible] = useState(true);
   const location = useLocation();
   const isBlogPage = location.pathname === '/blog';
+  const isAboutPage = location.pathname === '/about';
 
   useEffect(() => {
     const handleScroll = () => {
@@ -27,6 +28,50 @@ const Navbar = () => {
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const navLinks = isAboutPage || isBlogPage ? [
+    { 
+      name: "Home", 
+      href: "/", 
+      onClick: () => window.location.href = '/' 
+    },
+    { 
+      name: "About", 
+      href: "/about", 
+      onClick: () => window.location.href = '/about' 
+    },
+    { 
+      name: "Blog", 
+      href: "/blog", 
+      onClick: () => window.location.href = '/blog' 
+    },
+  ] : [
+    { 
+      name: "Testimonials", 
+      href: isBlogPage ? "/#testimonials" : "#testimonials", 
+      onClick: () => scrollToSection('testimonials') 
+    },
+    { 
+      name: "Packages", 
+      href: isBlogPage ? "/#packages" : "#packages", 
+      onClick: () => scrollToSection('packages') 
+    },
+    { 
+      name: "FAQ", 
+      href: isBlogPage ? "/#faq" : "#faq", 
+      onClick: () => scrollToSection('faq') 
+    },   
+    { 
+      name: "About", 
+      href: "/about", 
+      onClick: () => window.location.href = '/about' 
+    },
+    { 
+      name: "Blog", 
+      href: "/blog", 
+      onClick: () => window.location.href = '/blog' 
+    },
+  ];
 
   const scrollToSection = (sectionId: string) => {
     if (isBlogPage) {
@@ -47,35 +92,6 @@ const Navbar = () => {
       setIsMenuOpen(false);
     }
   };
-
-  const navLinks = [
- 
-    { 
-      name: "Testimonials", 
-      href: isBlogPage ? "/#testimonials" : "#testimonials", 
-      onClick: () => scrollToSection('testimonials') 
-    },
-    { 
-      name: "Packages", 
-      href: isBlogPage ? "/#packages" : "#packages", 
-      onClick: () => scrollToSection('packages') 
-    },
-    { 
-      name: "FAQ", 
-      href: isBlogPage ? "/#faq" : "#faq", 
-      onClick: () => scrollToSection('faq') 
-    },   
-    { 
-      name: "About", 
-      href: "/about", 
-      onClick: () => window.location.href = '/about'
-    },
-    { 
-      name: "Blog", 
-      href: "/blog", 
-      onClick: () => window.location.href = '/blog' 
-    },
-  ];
 
   return (
     <div className="fixed top-0 left-0 right-0 z-50">
